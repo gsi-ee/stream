3b63c016b01873061b32c088687f0984ba8e2c53
Sergey Linev <S.Linev@gsi.de>
2014-01-16 12:35:04 +0000

    1. Introduce EventStore class, which should be used to store events and (or)    results of events processing 2. Provide in go4engine TTreeStore, which implements ROOT TTree for the store.    In fact, TTree syntax will be used for EventStore 3. Simplify go4engine - keep only first step, remove analysis and second step. 4. Refactorize cern-gem12 and get4test applications, move complete code    into second.C script. 5. In mami-jan14 application provide example how event store can be used.    Produced tree can be directly used in go4 or ROOT.
    git-svn-id: https://subversion.gsi.de/go4/app/stream@1112 302e7798-3865-0410-85c9-d38765bbeff6

---
+++ go4engine/TStreamEvent.h
@@ -0,0 +1,22 @@
+#ifndef TSTREAMEVENT_H
+#define TSTREAMEVENT_H
+
+#include "TGo4EventElement.h"
+
+#include "base/Event.h"
+
+class TStreamEvent : public TGo4EventElement,
+                     public base::Event
+{
+   public:
+
+      TStreamEvent() : TGo4EventElement(), base::Event() {}
+
+      TStreamEvent(const char* name)  : TGo4EventElement(name), base::Event() {}
+
+      virtual ~TStreamEvent() {}
+
+      ClassDef(TStreamEvent,1)
+};
+
+#endif
