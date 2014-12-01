097390f142fd1a24ffd27c66366001f6afc3a4d0
Sergey Linev <S.Linev@gsi.de>
2014-12-01 15:08:01 +0000

    test with get4 simulation data
    git-svn-id: https://subversion.gsi.de/go4/app/stream@1291 302e7798-3865-0410-85c9-d38765bbeff6

--- applications/get4mbs/first.C
+++ applications/get4mbs/first.C
@@ -5,7 +5,7 @@ void first() {
    // this is just for MBS readout testing
 
    // create processor  get4mask, 32bit mode, tot multiplier
-   get4::MbsProcessor* proc = new get4::MbsProcessor(0x3, false, 1);
+   get4::MbsProcessor* proc = new get4::MbsProcessor(0x1, true, 8);
 
    // proc->SetAutoCalibr(100000);
 
@@ -18,9 +18,9 @@ void first() {
    proc->AddRef(0, 0, true,   0, 2, true,   200, -1000., 9000.);
    proc->AddRef(0, 0, true,   0, 3, true,   200, -1000., 9000.);
 
-   proc->AddRef(0, 0, true,   1, 1, true,   200, -1000., 9000.);
-   proc->AddRef(0, 0, true,   1, 2, true,   200, -1000., 9000.);
-   proc->AddRef(0, 0, true,   1, 3, true,   200, -1000., 9000.);
+   //proc->AddRef(0, 0, true,   1, 1, true,   200, -1000., 9000.);
+   //proc->AddRef(0, 0, true,   1, 2, true,   200, -1000., 9000.);
+   //proc->AddRef(0, 0, true,   1, 3, true,   200, -1000., 9000.);
 
 }
 
