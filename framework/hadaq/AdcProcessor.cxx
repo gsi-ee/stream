11e74f7b6ef8faedb8a46ae976daf13acc3a6b58
Andreas Neiser <neiser@kph.uni-mainz.de>
2015-06-11 11:48:59 +0000

    trb3adc: Huge changes for new firmware including TDC
    git-svn-id: https://subversion.gsi.de/go4/app/stream@1527 302e7798-3865-0410-85c9-d38765bbeff6

--- framework/hadaq/AdcProcessor.cxx
+++ framework/hadaq/AdcProcessor.cxx
@@ -16,35 +16,31 @@ using namespace std;
 vector<double> hadaq::AdcProcessor::storage; 
 
 hadaq::AdcProcessor::AdcProcessor(TrbProcessor* trb, unsigned subid, unsigned numchannels, double samplingPeriod) :
-   SubProcessor(trb, "ADC_%04x", subid),
+   TdcProcessor(trb, subid, 2, 0, "ADC_%04X"), // TDC channels 2, edgeMask=0 means just rising edge
    fSamplingPeriod(samplingPeriod),
    fStoreVect(),
    pStoreVect(0)
 {
+  
    fChannels = 0;
-
+   
    if (HistFillLevel() > 1) {
-      fKinds = MakeH1("Kinds", "Messages kinds", 16, 0, 16, "kinds");
-      fChannels = MakeH1("Channels", "Messages per channels", numchannels, 0, numchannels, "ch");
+      fKinds = MakeH1("ADCKinds", "Messages kinds", 16, 0, 16, "kinds");
+      fChannels = MakeH1("ADCChannels", "Messages per channels", numchannels, 0, numchannels, "ch");
    }
 
    for (unsigned ch=0;ch<numchannels;ch++) {
       ChannelRec rec; 
-      SetSubPrefix("Ch", ch);
-      rec.fValues = MakeH1("Values","Distribution of values (unsigned)", 1<<10, 0, 1<<10, "value");
-      rec.fWaveform = MakeH2("Waveform", "Integrated Waveform", 512, 0, 512, 1<<11, -(1<<10), 1<<10, "sample;value");
-      rec.fRawDiffTiming = MakeH1("RawDiffTiming","Timing difference",10000,-500,500,"t / ns");      
-      rec.fSamplesSinceTrigger = MakeH1("SamplesSinceTrigger","Samples since trigger", 512, 0, 512, "#samples");
-      rec.fIntegral = MakeH1("Integral","Summed integral",10000,0,10000,"integral");
-      rec.fCFDSamples = MakeH2("CFDSamples","Samples of the zero crossing",2,0,2,1000,-500,500,"crossing;value");
-      rec.fCFDDiffTiming = MakeH1("CFDDiffTiming","Timing difference",10000,-500,500,"t / ns");
-      rec.fEdgeSamples = MakeH2("EdgeSamples","Samples of the leading edge",4,0,4,1000,-500,500,"egde;value");
-      rec.fEdgeDiffTiming = MakeH1("EdgeDiffTiming","Timing difference",10000,-500,500,"t / ns");
-      rec.fSamples1 =  MakeH2("Samples1","Samples of zeroX and leading edge",6,0,6,1000,-500,500,"sample;value");
-      rec.fSamples2 =  MakeH2("Samples2","Samples of zeroX and leading edge",6,0,6,1000,-500,500,"sample;value");      
+      SetSubPrefix("ADCCh", ch);
+      rec.fValues = MakeH1("ADCValues","Distribution of values (unsigned)", 1<<10, 0, 1<<10, "value");
+      rec.fWaveform = MakeH2("ADCWaveform", "Integrated Waveform", 512, 0, 512, 1<<11, -(1<<10), 1<<10, "sample;value");
+      rec.fIntegral = MakeH1("ADCIntegral","Summed integral",10000,0,10000,"integral");
+      rec.fSamples = MakeH2("ADCSamples","Samples of the zero crossing",2,0,2,1000,-500,500,"crossing;value");
+      rec.fDiffTiming = MakeH1("ADCDiffTiming","Timing difference",10000,-500,500,"t / ns");
       SetSubPrefix();
       fCh.push_back(rec);
    }
+   // fCh.size() equals now numchannels...
 }
 
 
@@ -57,300 +53,49 @@ void hadaq::AdcProcessor::SetDiffChannel(unsigned ch, int diffch)
    fCh[ch].diffCh = diffch;
 }
 
-double calc_variance(const vector<double>& v) {
-   double sum = 0;
-   double sum2 = 0;
-   for(size_t i=0;i<v.size();i++) {
-      sum += v[i];
-      sum2 += v[i]*v[i];
-   }
-   sum  /= v.size();
-   sum2 /= v.size();
-   const double var = sum2 - sum*sum;
-   return var;
-}
-
-double calc_a_chi2(const vector<double>& xx, const vector<double>& yy, 
-                 const vector<double>& sx, const vector<double>& sy, 
-                 double& a,
-                 const double b_angle
-                 ) {
-   const size_t n = xx.size();
-   const double b = tan(b_angle);
-   // calculate a and weights ww
-   vector<double> ww(n);
-   double suma=0, sumw=0;
-   for(size_t i=0;i<n;i++) {
-      double ww_ = b*sx[i]*b*sx[i] + sy[i]*sy[i];
-      ww_ = 1/ww_;
-      ww[i] = ww_;
-      sumw += ww_;
-      suma += ww[i]*(yy[i]-b*xx[i]);
-   }
-   a = suma/sumw;
-   // calc chi2 based on a and b
-   double chi2 = 0;
-   for(size_t i=0;i<n;i++) {
-      const double num = yy[i]-a-b*xx[i];
-      chi2 += num*num*ww[i];
-   }
-   return chi2;
-}
-
-
-
-double find_chi2_minimum(
-      const vector<double>& xx, const vector<double>& yy, 
-      const vector<double>& sx, const vector<double>& sy, 
-      double& a,
-      double b_angle_a, double b_angle_b, double& b_angle
-      )
-{
-   const double eps = 1e-8;
-   const double t = 1e-4;
-   
-   double c;
-   double d;
-   double e;
-   double fu;
-   double fv;
-   double fw;
-   double fx;
-   double m;
-   double p;
-   double q;
-   double r;
-   double sa;
-   double sb;
-   double t2;
-   double tol;
-   double u;
-   double v;
-   double w;
-
-   c = 0.5 * ( 3.0 - sqrt ( 5.0 ) );
-   
-   sa = b_angle_a;
-   sb = b_angle_b;
-   b_angle = sa + c * ( b_angle_b - b_angle_a );
-   w = b_angle;
-   v = w;
-   e = 0.0;
-   fx = calc_a_chi2(xx,yy,sx,sy,a, b_angle);
-   fw = fx;
-   fv = fw;
-   
-   for ( ; ; )
-   { 
-      m = 0.5 * ( sa + sb ) ;
-      tol = eps * fabs ( b_angle ) + t;
-      t2 = 2.0 * tol;
-
-      if ( fabs ( b_angle - m ) <= t2 - 0.5 * ( sb - sa ) )
-      {
-         break;
-      }
-
-      r = 0.0;
-      q = r;
-      p = q;
-      
-      if ( tol < fabs ( e ) )
-      {
-         r = ( b_angle - w ) * ( fx - fv );
-         q = ( b_angle - v ) * ( fx - fw );
-         p = ( b_angle - v ) * q - ( b_angle - w ) * r;
-         q = 2.0 * ( q - r );
-         if ( 0.0 < q )
-         {
-            p = - p;
-         }
-         q = fabs ( q );
-         r = e;
-         e = d;
-      }
-      
-      if ( fabs ( p ) < fabs ( 0.5 * q * r ) && 
-           q * ( sa - b_angle ) < p && 
-           p < q * ( sb - b_angle ) )
-      {
-
-         d = p / q;
-         u = b_angle + d;
-
-         if ( ( u - sa ) < t2 || ( sb - u ) < t2 )
-         {
-            if ( b_angle < m )
-            {
-               d = tol;
-            }
-            else
-            {
-               d = - tol;
-            }
-         }
-      }
-
-      else
-      {
-         if ( b_angle < m )
-         {
-            e = sb - b_angle;
-         }
-         else
-         {
-            e = sa - b_angle;
-         }
-         d = c * e;
-      }
-
-      if ( tol <= fabs ( d ) )
-      {
-         u = b_angle + d;
-      }
-      else if ( 0.0 < d )
-      {
-         u = b_angle + tol;
-      }
-      else
-      {
-         u = b_angle - tol;
-      }
-      
-      fu = calc_a_chi2(xx,yy,sx,sy,a, u);
-
-      if ( fu <= fx )
-      {
-         if ( u < b_angle )
-         {
-            sb = b_angle;
-         }
-         else
-         {
-            sa = b_angle;
-         }
-         v = w;
-         fv = fw;
-         w = b_angle;
-         fw = fx;
-         b_angle = u;
-         fx = fu;
-      }
-      else
-      {
-         if ( u < b_angle )
-         {
-            sa = u;
-         }
-         else
-         {
-            sb = u;
-         }
-         
-         if ( fu <= fw || w == b_angle )
-         {
-            v = w;
-            fv = fw;
-            w = u;
-            fw = fu;
-         }
-         else if ( fu <= fv || v == b_angle || v == w )
-         {
-            v = u;
-            fv = fu;
-         }
-      }
-   }
-   return fx;
-}
-
-void fitxy(const vector<double>& x_, const vector<double>& y_, 
-           const vector<double>& sigy, 
-           double& a, double& b) {
-   double S   = 0;
-   double Sx  = 0;
-   double Sy  = 0;
-   double Sxx = 0;
-   double Sxy = 0;
-   for(size_t i=0;i<y_.size();i++) {
-      const double s = sigy[i];
-      const double s2 = s*s;
-      const double x  = x_[i];
-      const double y  = y_[i];
-      S   += 1/s2;
-      Sx  += x/s2;
-      Sy  += y/s2;
-      Sxx += x*x/s2;
-      Sxy += x*y/s2;
-   }
-   const double D = S*Sxx-Sx*Sx;
-   a = (Sxx*Sy - Sx*Sxy)/D;
-   b = (S*Sxy-Sx*Sy)/D;
-}
-
-void fitxye(const vector<double>& x_,   const vector<double>& y_, 
-            const vector<double>& sigx, const vector<double>& sigy,
-            double& a, double& b) {
-   double varx = calc_variance(x_);
-   double vary = calc_variance(y_);
-   double scale = sqrt(varx/vary);
-   const size_t n = x_.size();
-   vector<double> xx(n), yy(n), sx(n), sy(n), ww(n);
-   for(size_t i=0;i<n;i++) {
-      xx[i] = x_[i];
-      yy[i] = y_[i]*scale;
-      sx[i] = sigx[i];
-      sy[i] = sigy[i]*scale;
-      ww[i] = sqrt(sx[i]*sx[i]+sy[i]*sy[i]);      
-   }
-   
-   // conventional fit for starting point of b
-   fitxy(xx,yy,sy,a,b);
-   
-   double b_angle = atan(b);
-   
-   // find minimum
-   find_chi2_minimum(xx,yy,sx,sy,a, 0, b_angle, b_angle);
-   
-   // unscale
-   a /= scale;
-   b = tan(b_angle)/scale;
-}
-
-double getfraction(const vector<short>& edges) {
-   const size_t n = edges.size();
-   vector<double> x_(n), y_(n), sx(n), sy(n);
-   for(size_t i=0; i<y_.size(); i++) {
-      x_[i] = i;
-      y_[i] = edges[i];
-      sx[i] = 0.5;
-      sy[i] = 0.1+0.01*edges[i];
-   }
-   double a, b;
-   //fitxy(x_, y_, sy, a, b);
-   fitxye(x_,y_,sx,sy,a,b);
-   return -a/b;
-}
-
 bool hadaq::AdcProcessor::FirstBufferScan(const base::Buffer& buf)
 {
    unsigned len = buf.datalen()/4;
    uint32_t* arr = (uint32_t*) buf.ptr();
-
+   
+   // search for ADC marker (also contains epoch counter of trigger hit)
+   // in order to split the whole buffer into TDC part and ADC part
+   unsigned ADC_trigger_epoch;
+   unsigned ADC_offset = 0;
+   for (unsigned n=0;n<len;n++) {
+      if(arr[n] >> 28 == 0x1) {
+         ADC_offset = n;
+         ADC_trigger_epoch = arr[n] & 0xfffffff; // lower 28 bits are epoch
+         break;
+      }
+   }
+   // marker not found or no TDC data found, skip this buffer 
+   if(ADC_offset==0) {
+      return false;
+   }
+   
+   // decode the TDC stuff
+   base::Buffer TDC_buf;
+   TDC_buf.makecopyof(buf.ptr(), 4*ADC_offset);
+   if(!TdcProcessor::FirstBufferScan(TDC_buf))
+      return false;
+   TdcProcessor::SecondBufferScan(TDC_buf);
+   
+   
+  
    // printf("First scan len %u\n", len);
    // BeforeFill(); // optional
    // use iterator only if context is important
 
-   // start decoding the samples
+   // start decoding the ADC data
    uint32_t nSample = 0; // number of msg from the same ADC channel, aka Sample
    uint32_t lastCh = 0;  // remember last used channel for dumping verbose data
-   int lastTriggerEpoch = -1; // remember last trigger epoch
-   vector<int> triggerEpochs;
+
    
    vector< vector<short> > raw_samples;
    raw_samples.resize(fCh.size());
    
-   for (unsigned n=0;n<len;n++) {
+   for (unsigned n=ADC_offset+1;n<len;n++) {
       AdcMessage msg(arr[n]);
 
       uint32_t kind = msg.getKind();
@@ -361,12 +106,9 @@ bool hadaq::AdcProcessor::FirstBufferScan(const base::Buffer& buf)
          uint32_t ch = msg.getCh(); // has same structure as verbose data word
          if(ch>=fCh.size())
             continue;
-         // there should be now 5 values after this header word
-         const unsigned expected_len = 5;
+         // there should be now 3 values after this header word
+         const unsigned expected_len = 3;
          if(n+expected_len>len)
-            continue;
-         // we should have seen some trigger epoch word
-         if(lastTriggerEpoch<0)
             continue;
 
          ChannelRec& r = fCh[ch]; // helpful shortcut
@@ -377,8 +119,8 @@ bool hadaq::AdcProcessor::FirstBufferScan(const base::Buffer& buf)
          const unsigned epochCounter =
                (((arr[n] >> 8) & 0xff) << 16)
                + ((arr[n+1] >> 16) & 0xffff);
-         const int samplesSinceTrigger = epochCounter - lastTriggerEpoch;
-         FillH1(r.fSamplesSinceTrigger, samplesSinceTrigger);
+         const int samplesSinceTrigger = epochCounter - ADC_trigger_epoch;
+         //FillH1(r.fSamplesSinceTrigger, samplesSinceTrigger);
 
          // integral 
          const short integral = arr[n+1] & 0xffff;
@@ -387,49 +129,17 @@ bool hadaq::AdcProcessor::FirstBufferScan(const base::Buffer& buf)
          // CFD timing
          const short valBeforeZeroX = (arr[n+2] >> 16) & 0xffff;
          const short valAfterZeroX = arr[n+2] & 0xffff;
-         const double fraction_CFD = (double)valBeforeZeroX/(valBeforeZeroX-valAfterZeroX);
-         const double fineTiming_CFD = (samplesSinceTrigger + fraction_CFD)*fSamplingPeriod;
-         r.timing_CFD = fineTiming_CFD;
-         FillH2(r.fCFDSamples, 0, valBeforeZeroX);
-         FillH2(r.fCFDSamples, 1, valAfterZeroX);
-         r.samples.clear();
-         r.samples.push_back(valBeforeZeroX);
-         r.samples.push_back(valAfterZeroX);
-         
-         // four samples of the "edge"
-         vector<short> edges;
-         edges.push_back(arr[n+4] & 0xffff);
-         edges.push_back((arr[n+4] >> 16) & 0xffff);
-         edges.push_back(arr[n+3] & 0xffff);
-         edges.push_back((arr[n+3] >> 16) & 0xffff); 
-         r.samples.insert(r.samples.end(), edges.begin(), edges.end());
-        
-         // "fit" the four points to straight line f(x) = a + bx
-
-         const double fineTiming_Edge = (samplesSinceTrigger + getfraction(edges))*fSamplingPeriod;
-                 
-         for(size_t i=0;i<edges.size();i++) 
-            FillH2(r.fEdgeSamples, i, edges[i]);
-         r.timing_Edge = fineTiming_Edge;
-         
+         const double fraction = (double)valBeforeZeroX/(valBeforeZeroX-valAfterZeroX);
+         const double fineTiming = (samplesSinceTrigger + fraction)*fSamplingPeriod;
+         r.timing= fineTiming;
+         FillH2(r.fSamples, 0, valBeforeZeroX);
+         FillH2(r.fSamples, 1, valAfterZeroX);
+     
          
          // don't forget to move forward
          n += expected_len;
       }
-      // kind==0xe is trigger epoch word of CFD firmware
-      else if(kind == 0xe) {
-         // the lower 24bit are the epoch counter as gray code
-         std::bitset<24> trigger_epoch_gray(arr[n]);
-         std::bitset<24> trigger_epoch_bin(0);
-         trigger_epoch_bin[23] = trigger_epoch_gray[23];
-         for(int i=22 ; i>=0 ; i--) {
-            // B(i) = B(i+1) xor G(i), see Wikipedia :)
-            trigger_epoch_bin[i] = trigger_epoch_bin[i+1] ^ trigger_epoch_gray[i];
-         }
-         lastTriggerEpoch = trigger_epoch_bin.to_ulong();
-         triggerEpochs.push_back(lastTriggerEpoch);
-      }
-      // kind==0 is verbose data word for both firmware versions
+      // kind==0 is waveform debug data word
       else if(kind == 0) {
          uint32_t ch = msg.getCh();
          short int value = msg.getValue();
@@ -451,109 +161,9 @@ bool hadaq::AdcProcessor::FirstBufferScan(const base::Buffer& buf)
          }
 
          FillH2(fCh[ch].fWaveform, nSample, value);
-         
-         raw_samples[ch].push_back(value);
       }
       // other kinds like PSA data or compressed ADC words unsupported for now
       // they are just ignored
-   }
-
-   // if (!fCrossProcess) AfterFill(); // optional
-
-   // analyze raw samples for first two channels
-   for(size_t ch=0;ch<raw_samples.size();ch++) {
-      if(ch>1)
-         continue;
-      // calc baseline
-      const vector<short>& samples = raw_samples[ch];
-      double baseline = 0;
-      size_t n=0;
-      const size_t cut = 100;
-      for(size_t i=cut;i<samples.size();i++) {
-         baseline += samples[i];
-         n++;
-      }
-      baseline /= n;
-      
-      std::vector<double> filtered(cut,baseline);
-      for(size_t i=4;i<cut-4;i++) {
-         filtered[i] = 
-               -0.086 * samples[i-2] 
-               +0.343 * samples[i-1]
-               +0.486 * samples[i+0]
-               +0.343 * samples[i+1]
-               -0.086 * samples[i+2];
-         
-//         filtered[i] = 
-//               +0.035 * samples[i-4]
-//               -0.128 * samples[i-3]
-//               +0.070 * samples[i-2] 
-//               +0.315 * samples[i-1]
-//               +0.417 * samples[i+0]
-//               +0.315 * samples[i+1]
-//               +0.070 * samples[i+2]
-//               -0.128 * samples[i+3]
-//               +0.035 * samples[i+4]
-//               ;
-         
-//         filtered[i] = samples[i];
-      }
-      
-      
-      //cout << baseline << endl;
-      const size_t delay = 1;
-      const int thresh = 32;
-      double cfd_prev;
-      for(size_t i=0;i<cut;i++) {
-         double sample = -(filtered[i]-baseline);
-         if(sample<thresh)
-            continue;
-         double sample_delay = -(filtered[i+delay]-baseline);
-         double cfd = 4*sample-3*sample_delay;
-         if(i>0) {
-            if(cfd_prev<0 && cfd>=0) {
-               
-               const double fraction_Raw = (double)cfd_prev/(cfd_prev-cfd);
-               fCh[ch].timing_Raw = (triggerEpochs[ch]+i+fraction_Raw)*fSamplingPeriod;
-               //cout << fCh[ch].timing_Raw << endl;
-            }
-         }         
-         cfd_prev = cfd;
-      }
-   }
-   
-   
-   // do some reference timing business
-   for(size_t ch=0;ch<fCh.size();ch++) {
-      const ChannelRec& c = fCh[ch];
-      if(c.diffCh<0)
-         continue;
-      const double diff_Raw = c.timing_Raw - fCh[c.diffCh].timing_Raw;      
-      const double diff_CFD = c.timing_CFD - fCh[c.diffCh].timing_CFD;
-      const double diff_Edge = c.timing_Edge - fCh[c.diffCh].timing_Edge;
-      
-      FillH1(c.fRawDiffTiming, diff_Raw);
-      FillH1(c.fCFDDiffTiming, diff_CFD);
-      FillH1(c.fEdgeDiffTiming, diff_Edge); 
-      
-      
-      base::H2handle h = diff_CFD<211 ? c.fSamples1 : c.fSamples2;
-      for(size_t i=0;i<c.samples.size();i++) 
-         FillH2(h, i, c.samples[i]);
-      
-      
-      
-      // only for ch==0
-//      if(ch!=0)
-//         continue;
-//      if(storage.size()<1000) {
-//         storage.push_back(diff_Edge);
-//      }
-//      else {
-//         double var = calc_variance(storage);
-//         //cout << "RMS: " << sqrt(var) << endl;
-//         storage.clear();
-//      }
    }
 
    return true;
--- include/hadaq/AdcProcessor.h
+++ include/hadaq/AdcProcessor.h
@@ -2,6 +2,7 @@
 #define HADAQ_ADCPROCESSOR_H
 
 #include "hadaq/SubProcessor.h"
+#include "hadaq/TdcProcessor.h"
 
 #include "hadaq/AdcMessage.h"
 #include "hadaq/AdcSubEvent.h"
@@ -11,77 +12,65 @@
 
 namespace hadaq {
 
-   class TrbProcessor;
+class TrbProcessor;
 
-   /** This is specialized sub-processor for ADC addon.
+/** This is specialized sub-processor for ADC addon.
     * Normally it should be used together with TrbProcessor,
     * which the only can provide data
     **/
 
-   class AdcProcessor : public SubProcessor {
-
-      friend class TrbProcessor;
-
-      protected:
-
-         struct ChannelRec {
-            base::H1handle fValues;          //! histogram of values distribution in channel
-            base::H2handle fWaveform;        //! histogram of integrated raw waveform of channel (debug)
-            base::H1handle fRawDiffTiming;   //! histogram of CFD fine time difference to other specified channel            
-            base::H1handle fSamplesSinceTrigger;            
-            base::H1handle fIntegral;        //! histogram of integrals from CFD feature extraction
-            base::H2handle fCFDSamples;      //! histogram of fine timings from CFD feature extraction
-            base::H1handle fCFDDiffTiming;   //! histogram of CFD fine time difference to other specified channel
-            base::H2handle fEdgeSamples;     //! histogram of leading edge samples used for timing
-            base::H1handle fEdgeDiffTiming;  //! histogram of edge fine time difference to other specified channel
-            base::H2handle fSamples1;
-            base::H2handle fSamples2;            
-            int diffCh;                      //! if not <0, specifies channel for fCFDDiffTiming
-            double timing_Raw;               //! the timing in ns, relative to trigger word. Set in FillHistograms()            
-            double timing_CFD;               //! the timing in ns, relative to trigger word. Set in FillHistograms()
-            double timing_Edge;              //! the timing in ns, relative to trigger word. Set in FillHistograms()
-            std::vector<short> samples;
-            ChannelRec() :
-               diffCh(-1),
-               timing_Raw(std::numeric_limits<double>::quiet_NaN()),               
-               timing_CFD(std::numeric_limits<double>::quiet_NaN()),
-               timing_Edge(std::numeric_limits<double>::quiet_NaN()),
-               samples()
-            {}
-         };
-
-         const double fSamplingPeriod;
-         
-         static std::vector<double> storage;
-         
-
-         base::H1handle fKinds;      //! kinds of messages
-         base::H1handle fChannels;   //! histogram with messages per channel
-         std::vector<ChannelRec>  fCh; //! histogram for individual channels
-
-         std::vector<hadaq::AdcMessage>   fStoreVect; //! dummy empty vector
-         std::vector<hadaq::AdcMessage>  *pStoreVect; //! pointer on store vector
-
-         virtual void CreateBranch(TTree*);
-
-      public:
-
-         AdcProcessor(TrbProcessor* trb, unsigned subid, unsigned numchannels = 48, 
-                      double samplingPeriod = 1000.0/64);
-         virtual ~AdcProcessor();
-
-         inline unsigned NumChannels() const { return fCh.size(); }
-         void SetDiffChannel(unsigned ch, int diffch);
-
-         /** Scan all messages, find reference signals
-          * if returned false, buffer has error and must be discarded */
-         virtual bool FirstBufferScan(const base::Buffer& buf);
-
-         /** Scan buffer for selecting messages inside trigger window */
-         virtual bool SecondBufferScan(const base::Buffer& buf);
-
-         virtual void Store(base::Event*);
+class AdcProcessor : public TdcProcessor {
+   
+   friend class TrbProcessor;
+   
+protected:
+   
+   struct ChannelRec {
+      base::H1handle fValues;          //! histogram of values distribution in channel
+      base::H2handle fWaveform;        //! histogram of integrated raw waveform of channel (debug)
+      base::H1handle fIntegral;        //! histogram of integrals from CFD feature extraction
+      base::H2handle fSamples;         //! histogram of fine timings from CFD feature extraction
+      base::H1handle fDiffTiming;      //! histogram of CFD fine time difference to other specified channel
+      int diffCh;                      //! if not <0, specifies channel for fCFDDiffTiming
+      double timing;               //! the timing in ns, relative to trigger word. Set in FillHistograms()
+      ChannelRec() :
+         diffCh(-1),
+         timing(std::numeric_limits<double>::quiet_NaN())
+      {}
    };
+   
+   const double fSamplingPeriod;
+   
+   static std::vector<double> storage;
+   
+   
+   base::H1handle fKinds;      //! kinds of messages
+   base::H1handle fChannels;   //! histogram with messages per channel
+   std::vector<ChannelRec>  fCh; //! histogram for individual channels
+   
+   std::vector<hadaq::AdcMessage>   fStoreVect; //! dummy empty vector
+   std::vector<hadaq::AdcMessage>  *pStoreVect; //! pointer on store vector
+   
+   virtual void CreateBranch(TTree*);
+   
+public:
+   
+   AdcProcessor(TrbProcessor* trb, unsigned subid, unsigned numchannels = 48, 
+                double samplingPeriod = 1000.0/80);
+   virtual ~AdcProcessor();
+   
+   inline unsigned NumChannels() const { return fCh.size(); }
+   void SetDiffChannel(unsigned ch, int diffch);
+   
+   /** Scan all messages, find reference signals
+          * if returned false, buffer has error and must be discarded */
+   virtual bool FirstBufferScan(const base::Buffer& buf);
+   
+   /** Scan buffer for selecting messages inside trigger window */
+   virtual bool SecondBufferScan(const base::Buffer& buf);
+   
+   virtual void Store(base::Event*);
+};
 
 }
 
