cd31b48de22ecc0554da910b2ee7f35fe27d67a9
Andreas Neiser <neiser@kph.uni-mainz.de>
2015-06-22 12:15:46 +0000

    trb3adc: Implement single channel timings
    git-svn-id: https://subversion.gsi.de/go4/app/stream@1606 302e7798-3865-0410-85c9-d38765bbeff6

--- applications/trb3adc/second_hall.C
+++ applications/trb3adc/second_hall.C
@@ -15,145 +15,166 @@
 using namespace std;
 
 class ADCProc : public base::EventProc {
-   protected:
+protected:
+   
+   string fTdcId;   
+   string fAdcId;   
+   string fTdcInCTSId;
+   
+   
+   base::H1handle  hDiffTime; 
+   base::H1handle  hAdcPhase;      
+ 
+   struct hChannel_t {
+       base::H1handle Timing;
+       base::H2handle TDCHitVsTiming;
+   };
+   
+   typedef map<unsigned, hChannel_t> hChannels_t;
+   hChannels_t hChannels;
+   
+   const static double samplingPeriod = 12.5;
+   
+public:
+   ADCProc(const char* procname, unsigned id, unsigned ctsid) :
+      base::EventProc(procname)
+   {
       
-      string fTdcId;   
-      string fAdcId;   
-      string fTdcInCTSId;
+      stringstream ss;
+      ss << hex;
       
-
-      //double      fHits[16]; //!< 16 channel, last hit in every channel
+      ss << "ADC_" << setw(4) << setfill('0') << id;
+      fAdcId = ss.str();
+      ss.str("");
       
-      base::H1handle  hDiffTime; 
-      base::H1handle  hAdcPhase;      
+      ss << "TDC_" << setw(4) << setfill('0') << id;
+      fTdcId = ss.str();
+      ss.str("");
       
-      const static double samplingPeriod = 12.5;
+      ss << "TDC_" << setw(4) << setfill('0') << ctsid;
+      fTdcInCTSId = ss.str();
+      ss.str("");
+      
+      cout << "Create " << GetName() << " with " 
+           << fAdcId << "/" << fTdcId 
+           << " with ref " << fTdcInCTSId << endl;
+      
+      hDiffTime = MakeH1("DiffTime","Timing Differences", 4000, -200, 200, "t / ns");
+      hAdcPhase = MakeH1("AdcPhase","Phase of external trigger to ADC clock", 1000, 0, 100, "t / ns");
+      // enable storing already in constructor
+      //SetStoreEnabled();
+   }
+   
+   
+   virtual void CreateBranch(TTree* t)
+   {
+      //t->Branch(GetName(), fHits, "hits[16]/D");
+   }
+   
+   virtual bool Process(base::Event* ev) 
+   {
+      const bool debug = false;
+      
+      
+      hadaq::AdcSubEvent* adc = 
+            dynamic_cast<hadaq::AdcSubEvent*> (ev->GetSubEvent(fAdcId));
+      
+      hadaq::TdcSubEvent* tdc = 
+            dynamic_cast<hadaq::TdcSubEvent*> (ev->GetSubEvent(fTdcId));
+      
+      hadaq::TdcSubEvent* cts = 
+            dynamic_cast<hadaq::TdcSubEvent*> (ev->GetSubEvent(fTdcInCTSId));
+      
+      if(adc==0 || tdc==0 || cts==0)
+         return false;
+      
+      
+      
+      // we may need one hit from each TDC, 
+      // so search the subevents for this
+      const double nan = std::numeric_limits<double>::quiet_NaN();
+      double tm_TDC=nan, tm_CTS=nan;
+      
+      double trigger = ev->GetTriggerTime();
+      if(debug)
+         cout << "trigger=" << trigger << endl;
+      
+      for(unsigned cnt=0;cnt<tdc->Size();cnt++) {
+         const hadaq::TdcMessageExt& ext = tdc->msg(cnt);
+         unsigned chid = ext.msg().getHitChannel();
+         double tm = ext.GetGlobalTime();
+         if(debug)
+            cout << "TDC ch=" << chid << " time=" << tm << endl;  
+         if(chid==1) {
+            tm_TDC = tm;
+            break;
+         }
+      }
+      for(unsigned cnt=0;cnt<cts->Size();cnt++) {
+         const hadaq::TdcMessageExt& ext = cts->msg(cnt);
+         unsigned chid = ext.msg().getHitChannel();
+         double tm = ext.GetGlobalTime();
+         if(debug)
+            cout << "CTS ch=" << chid << " time=" << tm << endl;  
+         if(chid==1) {
+            tm_CTS = tm;
+            break;
+         }
+      }
+      
+      if(!(isfinite(tm_TDC) && isfinite(tm_CTS))) 
+         return false;
+      
+      // convert back to ns from here
+      tm_TDC  *= 1e9;
+      tm_CTS  *= 1e9;
+      
+      double ADC_phase = tm_TDC-tm_CTS;
+      FillH1(hAdcPhase, ADC_phase);
+      
+      
+      //cout << adc->Size() << endl;
+      
+      for(unsigned cnt=0;cnt<adc->Size();cnt++) {
+         const hadaq::AdcMessage& msg = adc->msg(cnt);
+         unsigned chid = msg.getCh();
+         SetSubPrefix("Ch", chid);
+         hChannels_t::iterator it = hChannels.find(chid);
+         hChannel_t hCh;
+         if(it == hChannels.end()) {
+            hCh.Timing = MakeH1("Timing","Timing to Trigger", 1000, -500, -100, "T' / ns");
+            hCh.TDCHitVsTiming = MakeH2("TDCHitVsTiming", "TDCHit vs. Timing", 500, 14, 30, 1000, -400, -200, "#delta_{2} / ns;T' / ns");
+            hChannels[chid] = hCh;
+         }
+         else {
+            hCh = it->second;
+         }
+         double fineTiming = 1e9*msg.fFineTiming;
+         fineTiming += ADC_phase;
+         
+         // due to some unknown but fixed delay between 
+         // the measurement of tm_TDC and tm_CTS, we need 
+         // to correct for an ADC epoch counter "glitch"
+         // the treshold does not seem to be constant...
+         // so this bug should actually be fixed in hardware 
+         if(tm_TDC>17.5) {
+             fineTiming -= samplingPeriod;
+         }
+         
+         if(debug)
+             cout << "ADC ch=" << chid
+                  << " timing=" << fineTiming
+                  << endl;
+         
+         FillH1(hCh.Timing, fineTiming);
+         FillH2(hCh.TDCHitVsTiming, tm_TDC, fineTiming);
+         
+      }
             
-   public:
-      ADCProc(const char* procname, unsigned id, unsigned ctsid) :
-         base::EventProc(procname)
-      {
-         
-         stringstream ss;
-         ss << hex;
-                  
-         ss << "ADC_" << setw(4) << setfill('0') << id;
-         fAdcId = ss.str();
-         ss.str("");
-         
-         ss << "TDC_" << setw(4) << setfill('0') << id;
-         fTdcId = ss.str();
-         ss.str("");
-         
-         ss << "TDC_" << setw(4) << setfill('0') << ctsid;
-         fTdcInCTSId = ss.str();
-         ss.str("");
-                  
-         cout << "Create " << GetName() << " with " 
-              << fAdcId << "/" << fTdcId 
-              << " with ref " << fTdcInCTSId << endl;
-
-         hDiffTime = MakeH1("DiffTime","Timing Differences", 4000, -200, 200, "t / ns");
-         hAdcPhase = MakeH1("AdcPhase","Phase of external trigger to ADC clock", 1000, 0, 100, "t / ns");
-         // enable storing already in constructor
-         //SetStoreEnabled();
-      }
-
-
-      virtual void CreateBranch(TTree* t)
-      {
-         //t->Branch(GetName(), fHits, "hits[16]/D");
-      }
-
-      virtual bool Process(base::Event* ev) 
-      {
-         const bool debug = false;
-         
-         
-         hadaq::AdcSubEvent* adc = 
-               dynamic_cast<hadaq::AdcSubEvent*> (ev->GetSubEvent(fAdcId));
-         
-         hadaq::TdcSubEvent* tdc = 
-               dynamic_cast<hadaq::TdcSubEvent*> (ev->GetSubEvent(fTdcId));
-                 
-         hadaq::TdcSubEvent* cts = 
-               dynamic_cast<hadaq::TdcSubEvent*> (ev->GetSubEvent(fTdcInCTSId));
-         
-         //if(adc==0 || tdc==0 || cts==0)
-         //   return false;
-         if(adc==0)
-            return false;
-         
-
-	 /*
-         // we may need one hit from each TDC, 
-         // so search the subevents for this
-         const double nan = std::numeric_limits<double>::quiet_NaN();
-         double tm_TDC=nan, tm_CTS=nan, tm_ADC1=nan, tm_ADC2=nan;
-         
-         double trigger = ev->GetTriggerTime();
-         if(debug)
-            cout << "trigger=" << trigger << endl;
-         
-         for(unsigned cnt=0;cnt<tdc->Size();cnt++) {
-            const hadaq::TdcMessageExt& ext = tdc->msg(cnt);
-            unsigned chid = ext.msg().getHitChannel();
-            double tm = ext.GetGlobalTime();
-            if(debug)
-               cout << "TDC ch=" << chid << " time=" << tm << endl;  
-            if(chid==1) {
-               tm_TDC = tm;
-               break;
-            }
-         }
-         for(unsigned cnt=0;cnt<cts->Size();cnt++) {
-            const hadaq::TdcMessageExt& ext = cts->msg(cnt);
-            unsigned chid = ext.msg().getHitChannel();
-            double tm = ext.GetGlobalTime();
-            if(debug)
-               cout << "CTS ch=" << chid << " time=" << tm << endl;  
-            if(chid==1) {
-               tm_CTS = tm;
-               break;
-            }
-         }
-         
-         if(!(isfinite(tm_TDC) && isfinite(tm_CTS))) 
-            return false;
-
-         double ADC_phase = tm_TDC-tm_CTS;
-         FillH1(hAdcPhase, ADC_phase);*/
-
-
-	 //cout << adc->Size() << endl;
-
-         for(unsigned cnt=0;cnt<adc->Size();cnt++) {
-            const hadaq::AdcMessage& msg = adc->msg(cnt);
-            unsigned chid = msg.getCh();
-            for(unsigned cnt_=cnt+1;cnt_<adc->Size();cnt_++) {
-               const hadaq::AdcMessage& msg_ = adc->msg(cnt_);
-               unsigned chid_ = msg_.getCh();
-               if(chid==chid_)
-                  continue;
-
-               double diff = msg.fFineTiming - msg_.fFineTiming;
-               diff *= 1e9; // convert to ns
-               // fill some histograms
-               FillH1(hDiffTime, diff);
-               double integral = msg.fIntegral;
-               if(debug)
-                  cout << "ADC ch1=" << chid
-                       << " ch2=" << chid_
-                       << " diff=" << diff
-                       << endl;
-            }
-         }
-
-
-         if(debug)
-            cout << endl;
-         return true;
-      }
+      if(debug)
+         cout << endl;
+      return true;
+   }
 };
 
 
