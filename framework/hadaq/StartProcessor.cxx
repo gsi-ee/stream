afc0f142fff9636a079e9b7f26435c6d3bc9d13d
Sergey Linev <S.Linev@gsi.de>
2018-07-05 10:55:58 +0000

    Introduce StartMonitor class. It dedicated to debug START detector in HADES
    git-svn-id: https://subversion.gsi.de/go4/app/stream@2128 302e7798-3865-0410-85c9-d38765bbeff6

---
+++ framework/hadaq/StartProcessor.cxx
@@ -0,0 +1,113 @@
+#include "hadaq/StartProcessor.h"
+
+#include "hadaq/definess.h"
+#include "hadaq/TrbIterator.h"
+#include "hadaq/TdcIterator.h"
+
+const double EPOCHLEN = 5e-9*0x800;
+
+
+hadaq::StartProcessor::StartProcessor() :
+   base::StreamProc("START", 0, false)
+{
+   mgr()->RegisterProc(this, base::proc_TRBEvent, 0);
+
+   fEvType = MakeH1("EvType", "Event type", 16, 0, 16, "id");
+   fEvSize = MakeH1("EvSize", "Event size", 500, 0, 500000, "bytes");
+   fSubevSize = MakeH1("SubevSize", "Subevent size", 500, 0, 5000, "bytes");
+
+   fEpochDiff = MakeH1("EpochDiff", "Difference in epochs", 100, -50, 50, "epoch");
+
+   fStartId = 0x8880;
+
+   fTdcMin = 0x5000;
+   fTdcMax = 0x5010;
+
+}
+
+hadaq::StartProcessor::~StartProcessor()
+{
+}
+
+/** Return time difference between epochs in seconds */
+double hadaq::StartProcessor::EpochTmDiff(unsigned ep1, unsigned ep2)
+{
+   return EpochDiff(ep1, ep2) * EPOCHLEN;
+}
+
+
+bool hadaq::StartProcessor::FirstBufferScan(const base::Buffer& buf)
+{
+   if (buf.null()) return false;
+
+   hadaq::TrbIterator iter(buf().buf, buf().datalen);
+
+   hadaqs::RawEvent* ev = 0;
+
+   unsigned evcnt = 0;
+
+   while ((ev = iter.nextEvent()) != 0) {
+
+      evcnt++;
+
+      DefFillH1(fEvType, (ev->GetId() & 0xf), 1.);
+      DefFillH1(fEvSize, ev->GetPaddedSize(), 1.);
+
+      hadaqs::RawSubevent* sub = 0;
+
+      while ((sub = iter.nextSubevent()) != 0) {
+
+         if (sub->GetId() != fStartId) continue;
+
+         DefFillH1(fSubevSize, sub->GetSize(), 1.);
+
+         unsigned ix = 0;           // cursor
+         unsigned trbSubEvSize = sub->GetSize() / 4 - 4;
+
+         while (ix < trbSubEvSize) {
+            //! Extract data portion from the whole packet (in a loop)
+            uint32_t data = sub->Data(ix++);
+
+            unsigned datalen = (data >> 16) & 0xFFFF;
+            unsigned dataid = data & 0xFFFF;
+
+            // ignore HUB HEADER
+            // if (dataid == hubid) continue;
+
+            bool istdc = (dataid>=fTdcMin) && (dataid<fTdcMax);
+
+            unsigned lastepoch = 0;
+
+
+            if (istdc) {
+
+               TdcIterator iter;
+               iter.assign(sub, ix, datalen);
+
+               hadaq::TdcMessage &msg = iter.msg();
+
+               while (iter.next()) {
+                  if (msg.isHitMsg()) {
+                     unsigned chid = msg.getHitChannel();
+                     //unsigned fine = msg.getHitTmFine();
+                     //unsigned coarse = msg.getHitTmCoarse();
+                     //bool isrising = msg.isHitRisingEdge();
+
+                  } else if (msg.isEpochMsg()) {
+                     unsigned epoch = iter.getCurEpoch();
+
+                     lastepoch = epoch;
+                  }
+               }
+            }
+
+            ix+=datalen;
+
+         } // while (ix < trbSubEvSize)
+
+      } // subevents
+
+   } // events
+
+   return true;
+}
---
+++ include/hadaq/StartProcessor.h
@@ -0,0 +1,52 @@
+#ifndef HADAQ_STARTPROCESSOR_H
+#define HADAQ_STARTPROCESSOR_H
+
+#include "base/StreamProc.h"
+
+#include "base/EventProc.h"
+
+namespace hadaq {
+
+/** This is specialized process for spill structures  */
+
+class StartProcessor : public base::StreamProc {
+
+protected:
+   base::H1handle fEvType;    ///< HADAQ event type
+   base::H1handle fEvSize;    ///< HADAQ event size
+   base::H1handle fSubevSize; ///< HADAQ sub-event size
+
+   base::H1handle fEpochDiff;  ///< epoch differences
+
+   unsigned fStartId; //< HUB with start detector
+
+   unsigned fTdcMin; // minimal TDC id
+   unsigned fTdcMax; // maximal TDC id
+
+   /** Hard difference between epochs */
+   inline unsigned EpochDiff(unsigned ep1, unsigned ep2) { return ep1 <= ep2 ? ep2 - ep1 : ep2 + 0x10000000 - ep1; }
+
+   /** Return time difference between epochs in seconds */
+   double EpochTmDiff(unsigned ep1, unsigned ep2);
+
+public:
+   StartProcessor();
+   virtual ~StartProcessor();
+
+   /** Scan all messages, find reference signals */
+   virtual bool FirstBufferScan(const base::Buffer &buf);
+
+   void SetStartId(unsigned id)
+   {
+      fStartId = id;
+   }
+
+   void SetTdcRange(unsigned min, unsigned max)
+   {
+      fTdcMin = min;
+      fTdcMax = max;
+   }
+};
+}
+
+#endif
